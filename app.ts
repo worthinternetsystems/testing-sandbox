0b053a329805657349e17ccd1256944e29825f81
Henry Penton <hpenton@worth.systems>
2023-05-17 13:51:11 +0100

    use in endpoint
    Co-authored-by: Henry Penton <hpenton@worth.systems>

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { InjectedTimeService } from "./service/injectedTimeService";
 
 const app = express();
 const port = 3000;
@@ -7,13 +8,13 @@ app.get("/untested", (req, res) => {
   res.send(new Date().toISOString());
 });
 
-
 app.get("/noninjected", (req, res) => {
   res.send(new Date().getTime().toString());
 });
 
 app.get("/injected", (req, res) => {
-  res.send(new Date().getTime().toString());
+  const injected = new InjectedTimeService(new Date());
+  res.send(injected.getCurrentIsoString());
 });
 
 app.listen(port, () => {
